f9f9819de90a2f22729c02194bdd3a53e81116f5
Ahmad Iqbal <secondemail1412@gmail.com>
2022-09-16 10:22:08 +0700

    fix routing and responsive

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import "./index.css";
@@ -12,9 +12,9 @@ import store from "./components/store/index.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
 
